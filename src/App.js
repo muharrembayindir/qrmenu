2b2470c651da9b87f2236e10d4676623c0c86b05
muba <muharrem.bayndr@gmail.com>
2025-07-12 09:20:44 +0300

    idk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MenuPage from './pages/MenuPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MenuPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MenuPage />} />
+    </Routes>
   );
 }
 
